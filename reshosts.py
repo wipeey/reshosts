595655d227d218222298b897c1f76d44b210888a
wipeey <>
2024-06-29 00:04:42 +0200

    Added arguments, hostname resolving and optimized code

--- reshosts.py
+++ reshosts.py
@@ -4,26 +4,44 @@ import sys
 import os
 import threading
 import platform
+import socket
+import argparse
 
 alive_hosts = []
 dead_hosts = []
+
+#Arguments handling
+parser = argparse.ArgumentParser()
+
+parser.add_argument('IP_ADDR') # Positional argument
+parser.add_argument('-v', '--verbose', action='store_true')
+
+args = parser.parse_args()
+
+def log(data):
+    if args.verbose:
+        print(data)
 
 
 # Ping a targeted IP address
 def ping(ip, operating_system):
     # Execute command for each thread depending on the operating system...
     if operating_system == 'Linux':
-        response = os.system(f'ping -c 1 -w 5 {ip} > /dev/null 2>&1') # Lunix command
+        response = os.system(f'ping -c 1 -w 5 {ip} > /dev/null 2>&1') # Linux command
     else:
         response = os.system(f'ping -c 1 -w 5 {ip} | find "TTL" > nul') # Windows command
-        
+
+    log(f'Currently checking host {ip}')
+
     # Check if host is up
     if response == 0:
         alive_hosts.append(ip)
+        log(f'{ip} is up!')
     else:
         dead_hosts.append(ip)
+        log(f'{ip} is down or unreachable!')
 
-        
+
 # Check if arg is in valid IP address format
 def is_ip_addr(arg):
     ip = arg.split('.')
@@ -54,6 +72,7 @@ PLEASE note that the script will only ping through last octet...
 
     print(error)
 
+
 # Print the results from the scan
 def results():
     result = "\nALIVE HOSTS:\n"
@@ -61,40 +80,36 @@ def results():
     # Handle proper syntax
     if alive_hosts:
         for host in alive_hosts:
-            if alive_hosts.index(host) == len(alive_hosts)-1:
-                result += f'{host}\n'
-            else:
-                result += f'{host}, '
+            try:
+                hostname = socket.gethostbyaddr(host)[0]
+            except Exception as e:
+                hostname = "Unknown host"
+
+            result += f'{host}, {hostname}\n'
     else:
-        result += '0\n'
+        result += 'None\n'
         
     result += f'\nDEAD HOSTS:\n{str(len(dead_hosts))}'
         
     print(result)
 
     
-def main():   
+def main():
     operating_system = platform.system()   
     threads_list = []
     supported_os = ['Windows', 'Linux']
-    args = sys.argv
     
     # Check if OS is supported
     if operating_system not in supported_os:
         print(f'Could not identify OS {operating_system}... Exiting')
         exit(1)
     
-    # Check if there are enough arguments
-    if len(args) < 2:
+    # Check if positional argument is in IP address format
+    if not is_ip_addr(args.IP_ADDR):
         error_arg()
         exit(1)
     
-    # Check if first argument is in IP address format
-    if not is_ip_addr(args[1]):
-        error_arg()
-        exit(1)
-    
-    ip = args[1].split('.')
+    ip = args.IP_ADDR.split('.')
     
     # Start a thread for each IP address to ping
     for i in range(0, 255):
@@ -102,9 +117,7 @@ def main():
         t = threading.Thread(target=ping, args=(host, operating_system))
         t.start()
         threads_list.append(t)
-    
-    print('Pinging...')
-    
+
     # Very important to avoid showing results before every thread finishes its execution
     for t in threads_list:
         t.join()
